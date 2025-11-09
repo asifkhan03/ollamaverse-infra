eeb224a0ebf8f0dfa7f44982cc77c0d600cd33a9
Asif Ahmad Khan <97660001+asifkhan03@users.noreply.github.com>
2025-11-09 11:20:33 +0000

    added Private RT in the route_tables modules

--- terraform-vpc/modules/vpc/route_tables.tf
+++ terraform-vpc/modules/vpc/route_tables.tf
@@ -16,3 +16,23 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+# Private Route Tables
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.vpcblock.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natblock[count.index].id
+  }
+
+  tags = merge({
+    Name = "${var.project}-${var.environment}-private-rt-${element(["a","b","c"], count.index)}"
+  }, var.tags)
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
