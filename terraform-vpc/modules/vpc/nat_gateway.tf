ce86213f34e08b47dc757f840259ceef2073d653
Asif Ahmad Khan <97660001+asifkhan03@users.noreply.github.com>
2025-11-09 11:19:14 +0000

    created NAT gw and placed it in the public subnets

--- terraform-vpc/modules/vpc/nat_gateway.tf
+++ terraform-vpc/modules/vpc/nat_gateway.tf
@@ -5,3 +5,13 @@ resource "aws_eip" "nat" {
     Name = "${var.project}-${var.environment}-nat-eip-${element(["a","b","c"], count.index)}"
   }, var.tags)
 }
+
+resource "aws_nat_gateway" "natblock" {
+  count         = length(var.public_subnet_cidrs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = merge({
+    Name = "${var.project}-${var.environment}-nat-gateway-${element(["a","b","c"], count.index)}"
+  }, var.tags)
+}
