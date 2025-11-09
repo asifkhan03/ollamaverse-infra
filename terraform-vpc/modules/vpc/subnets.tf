12cc1dcdd8131923e4c85cf3d658ddde376ad741
Asif Ahmad Khan <97660001+asifkhan03@users.noreply.github.com>
2025-11-09 07:54:45 +0000

    added the data subnet in the subnets.tf

--- terraform-vpc/modules/vpc/subnets.tf
+++ terraform-vpc/modules/vpc/subnets.tf
@@ -31,3 +31,17 @@ resource "aws_subnet" "private" {
     Name = "${var.project}-${var.environment}-private-subnet-${element(["a","b","c"], count.index)}"
   })
 }
+
+
+# Data Subnets
+resource "aws_subnet" "data" {
+  count             = length(var.data_subnet_cidrs)
+  vpc_id            = aws_vpc.vpcblock.id
+  cidr_block        = var.data_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(var.tags, {
+    Type = "Data"
+    Name = "${var.project}-${var.environment}-data-subnet-${element(["a","b","c"], count.index)}"
+  })
+}
