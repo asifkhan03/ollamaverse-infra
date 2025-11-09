d7960d0077b17e2bb249dd2b19620085064b64bc
Asif Ahmad Khan <97660001+asifkhan03@users.noreply.github.com>
2025-11-09 07:43:11 +0000

    added the vpc CIDR and subnet CIDR in the dev env. main.tf

--- terraform-vpc/environments/dev/main.tf
+++ terraform-vpc/environments/dev/main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "aws" {
   region  = "ap-south-1"  
 }
+
+module "vpc" {
+  source              = "../../modules/vpc"
+  environment         = "dev"
+  project             = "ollamaverse"
+  vpc_cidr            = "10.10.0.0/16"
+  public_subnet_cidrs = ["10.10.0.0/20", "10.10.16.0/20", "10.10.32.0/20"]
+  private_subnet_cidrs = ["10.10.48.0/20", "10.10.64.0/20", "10.10.80.0/20"]
+  data_subnet_cidrs    = ["10.10.96.0/20", "10.10.112.0/20", "10.10.128.0/20"]
+  tags = {
+    Owner = "Asif"
+  }
+}
