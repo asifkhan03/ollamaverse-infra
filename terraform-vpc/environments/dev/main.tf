c1904c63413a457e4a5a6984b86a7e03137a51f4
Asif Ahmad Khan <97660001+asifkhan03@users.noreply.github.com>
2025-11-09 11:30:42 +0000

    added aws resion

--- terraform-vpc/environments/dev/main.tf
+++ terraform-vpc/environments/dev/main.tf
@@ -16,6 +16,7 @@ module "vpc" {
   source              = "../../modules/vpc"
   environment         = "dev"
   project             = "ollamaverse"
+  aws_region          = "ap-south-1"
   vpc_cidr            = "10.10.0.0/16"
   public_subnet_cidrs = ["10.10.0.0/20", "10.10.16.0/20", "10.10.32.0/20"]
   private_subnet_cidrs = ["10.10.48.0/20", "10.10.64.0/20", "10.10.80.0/20"]
